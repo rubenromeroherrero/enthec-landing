fdf6cc72cbb4f910d9a2a4eb26af8e2c45132763
Rubén Romero Herrero <81327181+rubenromeroherrero@users.noreply.github.com>
2021-09-04 12:22:45 +0200

    responsive web and menu header with onclick

--- planet-express/src/app/core/components/header/header.component.ts
+++ planet-express/src/app/core/components/header/header.component.ts
@@ -3,13 +3,19 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  activated: boolean = false;
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  showMenu(): void {
+    if (this.activated === false) {
+      this.activated = true;
+    } else {
+      this.activated = false;
+    }
   }
-
 }
